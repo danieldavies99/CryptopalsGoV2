0cf056c0987e3ed256bc67dff1f60fd606822ae4
danieldavies99 <danieldavies99@gmail.com>
2024-06-29 18:32:53 +0100

    clear main.go

--- main.go
+++ main.go
@@ -9,29 +9,6 @@ import (
 )
 
 func main() {
-	fileContent, err := ioutil.ReadFile("./10.txt")
-	if err != nil {
-		panic(err)
-	}
-	cipherText, err := base64.Base64Decode(fileContent)
-	if err != nil {
-		panic(err)
-	}
-	decoded, err := cbc.CBCDecrypt(
-		cipherText,
-		[]byte("YELLOW SUBMARINE"),
-		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	)
-	if err != nil {
-		panic(err)
-	}
 
-	decoded = pkcs7.StripPadding(decoded)
-
-
-	println("Decoded: ", string(decoded))
 }
 
-// Sometimes I feel
-//  like life is ju
-// st a game fr fr.
